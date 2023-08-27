b3cd0afa00e8654d72090852aa0e9ab26002da31
Wonjun <jwj51720@gmail.com>
2023-08-27 21:03:20 +0900

    헤드라인 뉴스의 제목과 링크 크롤링

--- dataset/headline_crawler.py
+++ dataset/headline_crawler.py
@@ -5,31 +5,42 @@ import pandas as pd
 import time
 from tqdm import tqdm
 import os
+import datetime
+import pytz
 
 
-def save_csv(news_infos, check_start, check_end, save_path):
-    print(f"---------------save {check_start} {check_end}---------------")
-    print(f"---------------news_infos len {len(news_infos)}---------------")
+def save_csv(news_infos, check_start, check_end, save_path, kst):
+    print(f"..저장하는 뉴스의 번호: {check_start}-{check_end}..")
+    print(f"..저장하는 뉴스의 개수: {len(news_infos)}..")
+    now = datetime.datetime.now(kst)
     if not os.path.exists(save_path):
         os.mkdir(save_path)
 
     df = pd.DataFrame(news_infos, columns=["title", "url"])
     df.to_csv(
-        os.path.join(save_path, f"news_info_{check_start}_{check_end}.csv"), index=False
+        os.path.join(
+            save_path,
+            f"headline_{len(news_infos)}_{now.year}-{now.month}-{now.day}_{now.hour}-{now.minute}-{now.second}.csv",
+        ),
+        index=False,
+    )
+    print(
+        f"저장 완료! 'news_{len(news_infos)}_{now.year}-{now.month}-{now.day}_{now.hour}-{now.minute}-{now.second}.csv'"
     )
     return list()
 
 
-def headline_crawler(delay, save_every, section, start_iter, end_iter, save_path):
+def headline_crawler(
+    delay, save_every, section, start_iter, end_iter, header, save_path
+):
+    kst = pytz.timezone("Asia/Seoul")
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     news_infos = []  # title, link
     check_start = start_iter
     # 2023년 8월 25일 기준 IT
     base_url = f"https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1={section}#&date=%2000:00:00&page="
-    headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
-    }
+    headers = {"user-agent": header}
 
     for i in tqdm(range(start_iter, end_iter + 1)):
         req = requests.get(f"{base_url}{i}", headers=headers)
@@ -44,13 +55,14 @@ def headline_crawler(delay, save_every, section, start_iter, end_iter, save_path
             news_infos.append(news_info)
 
         if (i * len(newses)) % save_every == 0:
-            news_infos = save_csv(news_infos, check_start, (i * len(newses)), save_path)
+            news_infos = save_csv(
+                news_infos, check_start, (i * len(newses)), save_path, kst
+            )
             check_start = i * len(newses)
 
         if delay:
             time.sleep(delay)
-
-    save_csv(news_infos, check_start, (i * len(newses)), save_path)
+    save_csv(news_infos, check_start, (i * len(newses)), save_path, kst)
 
 
 if __name__ == "__main__":
@@ -59,7 +71,11 @@ if __name__ == "__main__":
         "-d", "--delay", default=0, type=int, help="Crawling delay를 설정해 block을 방지합니다."
     )
     parser.add_argument(
-        "-s", "--save_every", default=10000, type=int, help="저장되는 주기를 설정합니다."
+        "-s",
+        "--save_every",
+        default=10000,
+        type=int,
+        help="뉴스 기사가 csv로 저장되는 개수를 설정합니다.",
     )
     parser.add_argument(
         "--section", default=105, type=int, help="네이버 뉴스 분야별 코드를 입력합니다. sid1=xxx"
@@ -68,10 +84,16 @@ if __name__ == "__main__":
         "--start_iter", default=1, type=int, help="Crawling 시작 페이지를 지정합니다."
     )
     parser.add_argument(
-        "--end_iter", default=10000, type=int, help="Crawling 종료 페이지를 지정합니다."
+        "--end_iter", default=50, type=int, help="Crawling 종료 페이지를 지정합니다."
     )
     parser.add_argument(
-        "--save_path", default="./navernews/", type=str, help="저장할 경로를 지정합니다."
+        "--header",
+        default="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36)",
+        type=str,
+        help="개인 header를 입력합니다. 입력하지 않으면 접속에 오류가 생길 수 있습니다.",
+    )
+    parser.add_argument(
+        "--save_path", default="./navernews/", type=str, help="csv를 저장할 경로를 지정합니다."
     )
     args = parser.parse_args()
     headline_crawler(
@@ -80,5 +102,6 @@ if __name__ == "__main__":
         args.section,
         args.start_iter,
         args.end_iter,
+        args.header,
         args.save_path,
     )
